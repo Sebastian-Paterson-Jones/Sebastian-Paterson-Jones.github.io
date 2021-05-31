b76d81524dbed3642e16c8745845849f1a4704d4
Sebastian-Paterson-Jones <sebastian.boo.pj@gmail.com>
2021-05-31 21:50:01 +0200

    update development env

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env sh
 # abort on errors
 set -e
+#update the development branch
+git add -A
+git commit -m 'update development env'
+git push -f https://github.com/Sebastian-Paterson-Jones/Sebastian-Paterson-Jones.github.io.git master:development
 # build
 yarn run build
 # navigate into the build output directory
@@ -10,5 +14,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f https://github.com/Sebastian-Paterson-Jones/Sebastian-Paterson-Jones.github.io.git master:gh-pages
+git push -f https://github.com/Sebastian-Paterson-Jones/Sebastian-Paterson-Jones.github.io.git master
 cd -
