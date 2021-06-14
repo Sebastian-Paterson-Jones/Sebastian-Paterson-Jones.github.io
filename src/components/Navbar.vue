8142a0ac59c8e26c827a540823e3dcba36bee094
Sebastian Paterson-Jones <sebastian.boo.pj@gmail.com>
2021-06-14 10:36:11 +0200

    added styling to the document

---
+++ src/components/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <header class="header" id="header">
+      <nav class="nav container">
+          <a href="#" class="nav_logo">Sebastian</a>
+
+          <div class="nav_menu" id="nav-menu">
+              <ul class="nav_list grid">
+                  <li class="nav_item">
+                      <a href="#home"><i class="uil uil-estate"></i> Home</a>
+                  </li>
+                  <li class="nav_item">
+                      <a href="#about"><i class="uil uil-user-square"></i>about</a>
+                  </li>
+                  <li class="nav_item">
+                      <a href="#projects"><i class="uil uil-file"></i>projects</a>
+                  </li>
+              </ul>
+          </div>
+      </nav>
+  </header>
+</template>
+
+<style scoped>
+@import '../assets/styles/styles.css';
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <NavBar></NavBar>
+</template>
+
+<script>
+import NavBar from '../components/Navbar.vue';
+export default {
+  name: 'Home',
+  components: {
+    NavBar
+  }
+}
+</script>
